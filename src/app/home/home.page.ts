3ac4fbf67fbc648f2cfcca9284f9d7c080113747
Nisal47 <nisalmudawatta@gmail.com>
2019-07-25 10:17:22 +0530

    geo location added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Map, latLng, tileLayer, Layer, marker, polyline } from 'leaflet';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
-  constructor() {}
+  map: Map;
+  startLocation;
+  line;
 
+
+  ionViewDidEnter() {
+    // this.leafletMap();
+    this.locate();
+  }
+  constructor(private geolocation: Geolocation) {}
+
+  leafletMap() {
+    this.map = new Map('mapId').setView([6.97427, 79.91637], 13);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+  }
+
+  leafletMap1(lat: string, lng: string) {
+    this.map = new Map('mapId').setView([lat, lng], 20);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+  }
+
+  locate(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+
+
+      this.leafletMap1('' + resp.coords.latitude, '' + resp.coords.longitude);
+      marker([resp.coords.latitude, resp.coords.longitude]).addTo(this.map);
+
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
 }
